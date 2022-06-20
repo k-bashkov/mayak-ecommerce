edda69daecea87051d44bade7968485f15572bf2
bashkov <bashkov17serg@gmail.com>
2022-06-20 16:42:33 +0700

    commit to first deplory

---
+++ src/main/java/com/example/ecommerce/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.model.CustomUserDetail;
+import com.example.ecommerce.model.User;
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByEmail(email);
+        user.orElseThrow(() -> new UsernameNotFoundException("User dont found"));
+        return user.map(CustomUserDetail::new).get();
+    }
+}
